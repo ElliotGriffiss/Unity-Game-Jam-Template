4a6e949bef2d120d138b0ca517a7257973efbbb8
Elliot <elliotgriffiss@gmail.com>
2022-06-25 11:35:02 +0100

    Implemented a character controller.

--- Game Jam Template/Assets/Scripts/CharacterController.cs
+++ Game Jam Template/Assets/Scripts/CharacterController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.EventSystems;
 using CustomDataTypes;
 
 public class CharacterController : MonoBehaviour
@@ -14,23 +15,43 @@ public class CharacterController : MonoBehaviour
     public static event Action OnPlayerCompleteLevel = delegate { };
 
     [Header("Player References")]
-    [SerializeField] private Animator PlayerAnimator;
+    [SerializeField] protected SpriteRenderer Sprite;
+    [SerializeField] private Animator Animator;
+    [SerializeField] protected Rigidbody2D Rigidbody;
     protected Vector3Int CurrentPosition;
+
+
+    [Header("Settings")]
+    [SerializeField] protected float health = 10;
+    [SerializeField] private float chargeSpeed;
+    [SerializeField] protected float speedHorizontal;
+    [SerializeField] protected float speedVertical;
+    [SerializeField] protected float invTime;
 
     private LevelData LevelData;
     private IEnumerator DeathSequence;
-    private bool PlayerHasControl = false;
+
+    protected float CurrentHealth;
+    protected float currentInvTime;
+    protected bool isInvincible;
+    private bool PlayerHasControl = true;
+
+    protected Vector2 inputValue;
+    protected Vector2 direction;
+    protected Vector2 EnemyKnockbackForce;
 
     public void UpdateCurrentLevel(LevelData levelData)
     {
         LevelData = levelData;
 
         CurrentPosition = LevelData.SpawnPoint;
-        PlayerAnimator.SetBool("PlayerDead", false);
         PlayerHasControl = true;
+    }
 
-        // This just makes sure everything is reset properly when a level loads.
-        CharacterController.OnPlayerDeath();
+    public void RespawnCharacter()
+    {
+        CurrentHealth = health;
+        // animator.SetBool("IsMoving", false);
     }
 
     private void Update()
@@ -41,24 +62,80 @@ public class CharacterController : MonoBehaviour
         }
     }
 
+    protected virtual void FixedUpdate()
+    {
+        if (EnemyKnockbackForce != Vector2.zero)
+        {
+            Rigidbody.AddForce(EnemyKnockbackForce, ForceMode2D.Impulse);
+            EnemyKnockbackForce = Vector2.zero;
+        }
+    }
+
     private void CheckForPlayerInput()
     {
-        if (Input.GetAxis("Horizontal") < 0)
-        {
+        Vector2 force = Vector2.zero;
 
-        }
-        else if (Input.GetAxis("Horizontal") > 0)
+        if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
         {
-
+            // move forward 
+            Rigidbody.AddForce(direction.normalized * chargeSpeed, ForceMode2D.Impulse);
         }
-        else if (Input.GetAxis("Vertical") > 0)
+
+        if (inputValue.y != 0f)
         {
-
+            //animator.SetBool("IsMoving", true);
+            force += Vector2.up * inputValue.y * speedVertical;
         }
-        else if (Input.GetAxis("Vertical") < 0)
+
+        if (inputValue.x != 0)
         {
-
+            //animator.SetBool("IsMoving", true);
+            force += (Vector2.right * inputValue.x * speedHorizontal);
         }
+
+        inputValue.x = Input.GetAxisRaw("Horizontal"); //Setting the x and y values of the "movement" var based on what keys are down
+        inputValue.y = Input.GetAxisRaw("Vertical"); //^^
+
+        Rigidbody.AddForce(force);
+        Invincible();
+    }
+
+    public virtual void HandleCollisonEnter(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && !isInvincible)
+        {
+            DamageComponent damage = collision.collider.GetComponent<DamageComponent>();
+
+            isInvincible = true;
+            currentInvTime = invTime;
+
+            Rigidbody.velocity = Vector3.zero;
+            Rigidbody.angularVelocity = 0f;
+            EnemyKnockbackForce = (transform.position - collision.transform.position).normalized * damage.KnockBackForce;
+            CurrentHealth -= damage.Damage;
+        }
+
+        if (CurrentHealth <= 0)
+        {
+            TriggerPlayerDeath();
+        }
+    }
+
+    protected virtual void Invincible()
+    {
+        if (isInvincible == true)
+        {
+            currentInvTime -= Time.deltaTime;
+        }
+        if (currentInvTime <= 0)
+        {
+            isInvincible = false;
+        }
+    }
+
+    private void UpdateCharacterUI()
+    {
+
     }
 
     private void TriggerPlayerDeath()
@@ -78,7 +155,7 @@ public class CharacterController : MonoBehaviour
 
         CharacterController.OnPlayerDeath();
         CurrentPosition = LevelData.SpawnPoint;
-        PlayerAnimator.SetBool("PlayerDead", false);
+        //PlayerAnimator.SetBool("PlayerDead", false);
         DeathSequence = null;
         PlayerHasControl = true;
     }
---
+++ Game Jam Template/Assets/Scripts/DamageComponent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageComponent : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody;
+    public float Damage = 0;
+    public float KnockBackForce = 0;
+}
